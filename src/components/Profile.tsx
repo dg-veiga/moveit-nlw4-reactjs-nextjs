6d92238dc2489eaf9e53b6f2a0434d832647b357
Diego Veiga <diego.gveiga@yahoo.com>
2021-06-24 22:10:24 -0300

    readme imgs

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,7 +7,7 @@ export function Profile() {
 
     return (
         <div className={styles.profileContainer}>
-            <img src="https://media-exp1.licdn.com/dms/image/C5603AQEZS1dJeGPvRA/profile-displayphoto-shrink_200_200/0/1540300287396?e=1619654400&v=beta&t=7akkWncPa6bYxxZH_5TeWke4pLYI6_8gAReVccN3Ig8" alt="Diego Veiga" />
+            <img src="/favicon.png" alt="My Profile Photo" />
             <div>
                 <strong>Diego Veiga</strong>
                 <p>
